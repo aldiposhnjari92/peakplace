812a10b9dbeb87289bdfdfeeca2cac7370b8b6e8
aldiposhnjari <aldiposhnjari@gmail.com>
2025-02-25 09:00:47 +0100

    Refactor Hero component to improve layout and styling; add welcome message and responsive design

--- app/(home)/_components/hero.tsx
+++ app/(home)/_components/hero.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='h-full min-h-[calc(100vh-68px)] flex flex-col'>
+      <div className='flex flex-col px-4 w-full max-w-[1400px] mx-auto text-center'>
+        <h1 className='lg:text-5xl font-bold'>Welcome</h1>
+      </div>
+    </section>
   )
 }
 
